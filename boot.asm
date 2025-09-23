ad6f6b1035a63b5c6743bb7a74b56424a8d0f6e4
Edward Bierens <143363343+edwardsqrt-1@users.noreply.github.com>
2025-09-23 17:14:37 -0500

    Create a function to call for clearing the screen; improve functionality for text printing call to include location and color

--- boot.asm
+++ boot.asm
@@ -1,6 +1,7 @@
 ; Boot sector actually starts at 0x7C00
 org 0x7C00
-_boot:
+
+_boot: ; Initializing everything and set extra segment to VGA text mode address
 
     ; Set extra segment as the segment for VGA text mode 80x25 by default
     mov ax, VGA_MEMORY_TEXT
@@ -16,30 +17,70 @@ _boot:
     ; Go to Entry Point
     jmp _starthere
 
-_printstr: ; Prints a string pointed to by the register BX
+_blankscreen: ; Function to blank the screen with the color value in al
 
-    ; Extract character and determine if null character
-    mov al, [bx]
-    cmp al, 0
-    jne _printstr_print
+    xor di, di
+
+    _blankscreen_clear:
+        ; Set all other characters in video memory to null
+        mov [es:di], byte 0x00
+        inc di
+
+        ; Set both the foreground and background to be black
+        mov [es:di], al
+        inc di
+
+        ; Determine when end of video memory has been reached
+        cmp di, 0xFA0 ; (80*25)*2 for two bytes per character
+        jne _blankscreen_clear
+
     ret
 
-_printstr_print: ; Part of _printstr, but it skips here to print the value if not zero
-    ; Place character in video memory
-    mov [es:di], byte al
-    inc di
+_printstr: ; Prints a string where string = bx, x = cl, y = ch, and color = dh
 
-    ; Give character a color (foreground = cyan and background = black)
-    mov [es:di], byte 0x3
-    inc di
+    ; Convert y value into multiples of 80 (80 columns) and store into di
+    ; Note that each character corresponds to 2 bytes; first for character and second for attribute
+    mov ax, cx
+    shr ax, 8
+    mov dl, 80
+    mul dl
+    shl ax, 1
+    mov di, ax 
 
-    ; Go to next character and repeat
-    inc bx
-    jmp _printstr
+    ; Convert x value into the value to translate di by
+    mov ax, cx
+    xor ah, ah
+    shl ax, 1
+    add di, ax
+
+    ; Set background color to blue
+    or dh, 0x10
+
+    ; Begin loop
+    _printstr_char:
+
+        ; Extract character and determine if null character
+        mov al, [bx]
+        cmp al, 0
+        jne _printstr_print
+        ret
+
+        _printstr_print: ; Part of _printstr, but it skips here to print the value if not zero
+
+            ; Place character in video memory
+            mov [es:di], al
+            inc di
+
+            ; Give character a color (foreground = yellow and background = blue)
+            mov [es:di], dh
+            inc di
+
+            ; Go to next character and repeat
+            inc bx
+            jmp _printstr_char
 
 
-; Entry point
-_starthere:
+_starthere: ; Entry point
 
     ; Turn off the blinking cursor that appears by default
     mov dx, 0x3D4
@@ -49,37 +90,34 @@ _starthere:
     mov al, 0x20
     out dx, al
 
+    ; Make background blue
+    mov al, 0x10
+    call _blankscreen
+    xor di, di
+
     ; Print out the UI-286 Construction Message
+    mov ch, 12
+    mov cl, 23
+    mov dh, 0xE
     mov bx, ui286
     call _printstr
 
+    ; Print out secondary message
+    mov ch, 14
+    mov cl, 31
+    mov dh, 0xA
+    mov bx, wait_str
+    call _printstr
+
     ; Go to halting loop when done
-    jmp _done
-
-; Finishing point 
-_done:
-
-    ; Set all other characters in video memory to null
-    mov [es:di], byte 0x0
-    inc di
-
-    ; Set both the foreground and background to be black
-    mov [es:di], byte 0x0
-    inc di
-
-    ; Determine when end of video memory has been reached
-    cmp di, 0xFA0 ; (80*25)*2 for two bytes per character
-    je _halt
-    jmp _done
-
-; Halts with a loading indicator
-_halt:
-
-    ; Choose end of first line
     mov di, 158
+    jmp _halt
 
-    ; Set the foreground color to be red but keep background as black
-    mov [es:di+1], byte 0x4
+
+_halt: ; Halts with a loading indicator
+
+    ; Set the foreground color to be cyan but keep background as blue
+    mov [es:di+1], byte 0x13
 
     ; Create loading symbol
     mov [es:di], byte '-'
@@ -87,7 +125,7 @@ _halt:
     mov [es:di], byte '|'
     mov [es:di], byte '/'
 
-    ; Repeat
+    ; Repeat until PC is powered off
     jmp _halt
 
 ; Data variables go here
@@ -95,8 +133,9 @@ _data:
 
     ; String for UI-286 Construction Message
     ui286   db  "UI-286 Construction In Progress...",0
+    wait_str    db  "Please stay tuned!",0
 
-    ; Fill up to bytes 511 and 512
+    ; Fill up to bytes 511 and 512 starting from current line
     times 510-($-$$) db 0
 
     ; MBR Boot signature (0x55 and 0xAA)
@@ -107,4 +146,4 @@ _data:
     ; times 1474048 db 0 ; 3.5" 1.44 MB Floppy
 
     ; Definitions go here
-    VGA_MEMORY_TEXT equ 0xB800
+    VGA_MEMORY_TEXT equ 0xB800 ; Memory address for VGA Text Mode 80x25
