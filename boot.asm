c6cfdcda5c430abad0967fbcc9f5cc7bd706c471
Edward Bierens <143363343+edwardsqrt-1@users.noreply.github.com>
2025-09-30 21:50:31 -0500

    Break the 512 byte barrier and be able to load more of the program through Assembly.

--- boot.asm
+++ boot.asm
@@ -1,4 +1,10 @@
-; Boot sector actually starts at 0x7C00
+; Definitions go here (are not in the program itself)
+VGA_MEMORY_TEXT     equ 0xB800 ; Memory address for VGA Text Mode 80x25
+STACK_ADDRESS       equ 0x7BFF
+STACK_SIZE_BYTES    equ 10
+KEYBOARD_ADDRESS    equ 0x041E
+
+; Boot sector is loaded by the computer in the address 0x7C00
 org 0x7C00
 
 ; Initializing everything and set extra segment to VGA text mode address
@@ -7,6 +13,9 @@ _boot:
     ; Set extra segment as the segment for VGA text mode 80x25 by default
     mov ax, VGA_MEMORY_TEXT
     mov es, ax
+
+    ; Save boot location
+    mov [boot_loc], dl
 
     ; Clear out all values
     xor ax,ax
@@ -83,7 +92,7 @@ _makesound:
     ; Exit
     ret
     
-; Prints a character in al with a color in dh
+; Prints a character at the current screen location in al with a color in dh
 _printchar:
 
     ; Place character in video memory
@@ -114,9 +123,6 @@ _printnum:
     xor ah, ah
     shl ax, 1
     add di, ax
-
-    ; Add blue background color
-    or dh, 0x90
 
     ; Free up dx register to hold the remainder (it contains the higher digits of integer), and move number to ax. Set bx to 10, and set cx to 0
     mov si, dx
@@ -181,9 +187,6 @@ _printstr:
     shl ax, 1
     add di, ax
 
-    ; Set background color to blue
-    or dh, 0x10
-
     ; Begin loop
     _printstr_char:
 
@@ -220,50 +223,181 @@ _starthere: ; Entry point
     call _blankscreen
     xor di, di
 
+    ; Print out memory message prompt
+    mov ch, 1
+    mov cl, 1
+    mov dh, 0x9C
+    mov bx, boot_str
+    call _printstr
+
+    ; Create a beep to show sound is working
+    mov bx, 131
+    mov si, 0x1
+    mov di, 0xE848
+    call _makesound
+    mov bx, 262
+    call _makesound
+    mov bx, 523
+    call _makesound
+    mov bx, 1046
+    call _makesound
+    mov bx, 2093
+    call _makesound
+    mov bx, 1046
+    call _makesound
+    mov bx, 523
+    call _makesound
+    mov bx, 262
+    call _makesound
+
+    ; Load next ten sectors that stores the rest of the bootloader
+    ; ah = command to read floppy
+    ; al = number of sectors to read
+    ; bx = offset of buffer (in this case, just the address of the buffer)
+    ; ch = cylinder to use
+    ; cl = sector to use
+    ; dh = head to use
+    ; dl = drive to read from where 0 = A:, 1 = B:, 2 = C:, and 3 = D:
+    ; es = higher order bits of address. Since we are loading this right after the boot sector at the value 0x7E00 (0x7C00 + 512 bytes), this is just 0
+    mov ah, 0x02
+    mov al, 10
+    mov bx, 0x7E00
+    xor ch, ch
+    mov cl, 2
+    xor dx, dx
+    mov es, dx
+    xor dh, dh
+    mov dl, [boot_loc]
+
+    ; Execute the command to load the sectors; if successful then proceed to the post-boot sector lines, but catch any error
+    int 0x13
+    jc floppy_load_fail
+    jmp starthere_stage2
+
+    ; In case an error arises, go here and after printing debugging information, halt
+    floppy_load_fail:
+
+        ; Display error code in ah if failed
+        mov bx, VGA_MEMORY_TEXT
+        mov es, bx
+        mov ch, 1
+        mov cl, 1
+        mov dh, 0x9C
+        mov bx, err_status
+        call _printstr 
+        mov ch, 1
+        mov cl, 25
+        shr ax, 8
+        mov bx, ax
+        call _printnum
+
+    ; Halt here
+    _nothing:
+        nop
+        nop
+        nop
+        jmp _nothing
+
+; Data variables go here
+_data:
+
+    ; String for UI-286 Construction Message
+    ui286   db  "UI-286 Construction In Progress...",0
+    wait_str    db  "Please stay tuned!",0
+    border  db  "============================================",0
+    err_status  db  "DISKETTE ERROR! Status:",0
+    boot_str    db  "Loading UI-286 into memory...",0
+    boot_done   db  "Done!"
+    boot_loc    db  0
+
+    ; Memory size related constants
+    sys_memory_p    db  "Memory Size: ",0
+    memsize dw  0x0000
+
+    ; (Preview) This is what the input prompt will look like
+    ;sys_key_p    db  "Input > ",0
+
+; Fill up to bytes 511 and 512 starting from current line
+times 510-($-$$) db 0
+
+; MBR Boot signature (0x55 and 0xAA)
+dw 0xAA55
+
+; Execution continues here after the magic number
+starthere_stage2:
+
+    ; Reset extra segment to point to video memory
+    mov ax, VGA_MEMORY_TEXT
+    mov es, ax
+
+    ; Print out memory message prompt
+    mov ch, 1
+    mov cl, 1
+    mov dh, 0x1C
+    mov bx, boot_str
+    call _printstr
+
+    ; Print out memory message prompt
+    mov ch, 3
+    mov cl, 1
+    mov dh, 0x1E
+    mov bx, boot_done
+    call _printstr
+
+    ; Artificial delay so that the user can see that it has finished
+    mov cx, 0x1E
+    mov dx, 0x8480
+    mov ah, 0x86
+    int 0x15
+
+    ; Make background blue (again)
+    mov al, 0x10
+    call _blankscreen
+    xor di, di
+
     ; Print out the UI-286 Construction Message
     mov ch, 12
     mov cl, 23
-    mov dh, 0xE
+    mov dh, 0x1E
     mov bx, ui286
     call _printstr
 
     ; Print out secondary message
     mov ch, 14
     mov cl, 31
-    mov dh, 0xA
+    mov dh, 0x1A
     mov bx, wait_str
     call _printstr
 
     ; Print out upper border
     mov ch, 8
     mov cl, 18
-    mov dh, 0xE
+    mov dh, 0x1E
     mov bx, border
     call _printstr
 
     ; Print out lower border
     mov ch, 18
     mov cl, 18
-    mov dh, 0xE
+    mov dh, 0x1E
     mov bx, border
     call _printstr
 
     ; Print out memory message prompt
     mov ch, 22
     mov cl, 30
-    mov dh, 0xB
+    mov dh, 0x1B
     mov bx, sys_memory_p
     call _printstr
 
-    ; Retreive/store the amount of RAM in the system (int 12 places the RAM size in KBs)
+    ; Retrieve/store the amount of RAM in the system (int 12 places the RAM size in KBs)
     int 0x12 ; BIOS call for RAM size; get it now while in real mode
-    inc ax ; Add one to include the 512 byte boot sector memory + 1 extra sector
     mov [memsize], ax
     mov ch, 22
     mov cl, 43
     mov bx, ax
     call _printnum ; Note: this is an integer, hence the need for an integer printing function
-
+    
     ; Add "KB" at the end of the number
     add di, 2
     mov al, 'K'
@@ -271,26 +405,23 @@ _starthere: ; Entry point
     mov al, 'B'
     call _printchar
 
-    ; Create a running rise of notes (note value in bx) as a speaker test
-    mov bx, 131
+    ; Different beep to show success
+    mov bx, 1000
     mov si, 0x3
     mov di, 0xD090
     call _makesound
-    mov bx, 262
-    call _makesound
-    mov bx, 523
-    call _makesound
-    mov bx, 1047
-    call _makesound
-    mov bx, 2093
-    call _makesound
-    mov bx, 3951
-    call _makesound
-    mov bx, 5643
+
+    ; Different beep to show success
+    mov bx, 1500
+    mov si, 0x4
+    mov di, 0xD090
     call _makesound
 
     ; Go to halting loop when done
     mov di, 398
+    mov cx, 0x7
+    mov dx, 0xA120
+    mov ah, 0x86
     jmp _halt
 
 ; Halts with a loading indicator
@@ -301,40 +432,22 @@ _halt:
 
     ; Create loading symbol
     mov [es:di], byte '-'
+    nop
+    int 0x15
     mov [es:di], byte '\'
+    nop
+    int 0x15
     mov [es:di], byte '|'
+    nop
+    int 0x15
     mov [es:di], byte '/'
+    nop
+    int 0x15
 
     ; Repeat until PC is powered off
     jmp _halt
 
-; Data variables go here
-_data:
 
-    ; String for UI-286 Construction Message
-    ui286   db  "UI-286 Construction In Progress...",0
-    wait_str    db  "Please stay tuned!",0
-    border  db  "============================================",0
-
-    ; Memory size related constants
-    sys_memory_p    db  "Memory Size: ",0
-    memsize dw  0x0000
-
-    ; (Preview) This is what the input prompt will look like
-    sys_key_p    db  "Input > ",0
-
-    ; Fill up to bytes 511 and 512 starting from current line
-    times 510-($-$$) db 0
-
-    ; MBR Boot signature (0x55 and 0xAA)
-    dw 0xAA55
-
-    ; Fill up rest of the floppy
-    times 1228288 db 0 ; 5.25" 1.2 MB Floppy
-    ;times 1474048 db 0 ; 3.5" 1.44 MB Floppy
-
-    ; Definitions go here (are not in the program itself)
-    VGA_MEMORY_TEXT     equ 0xB800 ; Memory address for VGA Text Mode 80x25
-    STACK_ADDRESS       equ 0x7BFF
-    STACK_SIZE_BYTES    equ 10
-    KEYBOARD_ADDRESS    equ 0x041E
+; Fill up rest of the floppy
+times 1228800-($-$$) db 0 ; 5.25" 1.2 MB Floppy
+;times 1474560-($-$$) db 0 ; 3.5" 1.44 MB Floppy
